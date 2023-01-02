857d83dc726b3d05c2340a86106254f1e3e0a453
AshrafPatel <4shr4f@live.co.uk>
2023-01-02 20:33:13 +0000

    Contacts Frontend Changed

--- Contacts/client/src/Background.js
+++ Contacts/client/src/Background.js
@@ -7,20 +7,74 @@ class Background extends Component {
     constructor(props){
       super(props);
       this.state = {
-        contacts: []
+        contacts: [],
+        isLoading: false,
+        form: false,
+        name: "",
+        email: ""
       }
+      this.handleClick = this.handleClick.bind(this);
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleClick() {
+      this.setState({
+        form:true
+      })
+    }
+
+    handleInputChange(event) {
+      const value = event.target.value;
+      const name = event.target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+
+    onSubmitHandler = (event) => {
+      fetch("https://localhost:5000/api/Contacts", {
+          method: "post",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              email: this.state.email,
+              name: this.state.name
+          })
+      })
+      fetch('https://localhost:5000/api/Contacts')
+          .then(response => response.json())
+          .then(contacts => this.setState({contacts}));
     }
 
     componentDidMount() {
+      try {
         fetch('https://localhost:5000/api/contacts')
           .then(response => response.json())
-          .then(contacts => this.setState({ contacts }));
+          .then(contacts => this.setState({contacts}));
+      } catch(e) {
+        console.error(e);
+        this.setState({isLoading: true})
+      }
     }
   
     render() {
       return (
         <div>
-          {this.state.contacts.map((contact, i) => <ContactCard contact={contact} key={i} />)}
+          <div style={{display: "flex"}}>
+            {this.state.isLoading ? "Error" : this.state.contacts.map((contact, i) => <ContactCard contact={contact} key={i} />)}
+          </div>
+          {this.state.form === false ? 
+          <button onClick={this.handleClick} className="myButton">Create</button> : 
+          <div>
+            <label name="email">Email</label>
+            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange}/>
+            <label name="name">Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange}/>
+            <br/>
+            <button type="submit" className="myButton" onClick={this.onSubmitHandler}>Create</button>
+          </div>}
         </div>
       )
     }
--- Contacts/client/src/ContactCard.js
+++ Contacts/client/src/ContactCard.js
@@ -1,31 +1,61 @@
 import "./Contact.css";
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class ContactCard extends Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {
-          action:"get"
+function ContactCard(props) { 
+  const [action, setAction] = useState(0);
+  const [name, setName] = useState(0);
+  const [email, setEmail] = useState(0);
+
+  const handleDelete = (e) => {
+    fetch('https://localhost:5000/api/Contacts/'+ props.contact.id,
+      { 
+        method: 'DELETE', 
+        headers: {
+        "Content-Type": "application/json"
         }
-    }
+      }
+    )
+    setAction("get")
+  };
 
-    render(props) {
-        return (
-        <div className="card">
-            <img src={"https://thispersondoesnotexist.com/image"} alt="Avatar" style={{width:100 + "%"}}/>
-            <div className="container">
-              <h4><b>{props.contact}</b></h4>
-              <p>{props}</p>
-              <div className="buttonContain">
-                <button className="buttonEdit">Edit</button>
-                <button>Delete</button>
-              </div>
-            </div>
+  const handleEdit = (e) => {
+    fetch('https://localhost:5000/api/Contacts/'+ props.contact.id,
+      { 
+        method: 'PUT', 
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          name: name
+        })
+      }
+    )
+    setAction("get")
+  };
+ 
+
+  return (
+    <div className="card">
+      <img src={"https://mjsolpac.com/wp-content/uploads/2017/03/person-placeholder.jpg"} alt="Avatar" style={{width:100 + "%"}}/>
+      <div className="container">
+        <h4><b>{props.contact.name}</b></h4>
+        <p>{props.contact.email}</p>
+        <div className="buttonContain">
+          {action === "edit" ? 
+          <div>
+            <label name="email">Email</label>
+            <input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+            <label name="name">Name</label>
+            <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/>
+            <button onClick={handleEdit} className="button">Submit</button>
+          </div> : 
+          <button onClick={() => setAction("edit")} className="button">Edit</button>}
+          {action === "delete" ? <button onClick={handleDelete} className="button deletebtn">Delete Confirm</button> : <button onClick={() => setAction("delete")} className="button deletebtn">Delete</button>}
         </div>
-        );
-    }
+      </div>
+    </div>
+  );
 }
-
 
 export default ContactCard;
